82ed59f34e2735a376f321848fe99c54b695efed
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-01-14 01:41:10 +0530

    Navbar,header and context component development.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
-  return (
-    <div>
-        <div>
-           <Link to='/'><img src={assets.logo} alt='' className='' /></Link> 
-        </div>
-        <div>
 
+    const {user} = useContext(AppContext)
+
+    const navigate = useNavigate()
+
+    return (
+        <div className='flex items-center justify-between py-4'>
+            <div>
+                <Link to='/'><img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40' /></Link>
+            </div>
+            <div>
+                {
+                user
+                    ?
+                    <div className='flex items-center gap-2 sm:gap-3'>
+                        <button onClick={()=>navigate('/buy')} className='flex items-center bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full gap-2 hover:scale-105 transition-all duration-500'>
+                            <img className='w-5 cursor-pointer' src={assets.credit_star} alt=''/>
+                            <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left : 50</p>
+                        </button>
+                        <p className='text-gray-600 max-sm:hidden pl-4'>Hi! Richard</p>
+                        <div className='relative group cursor-pointer'>
+                            <img className='w-10 drop-shadow' src={assets.profile_icon} alt=''/>
+                            <div className='absolute hidden group-hover:block top-0 right-0 pt-12 z-10 text-black rounded'>
+                                <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                                    <li className='py-1 px-2 cursor-pointer pr-10'>
+                                        Logout
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className='flex items-center gap-2 sm:gap-5'>
+                        <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                        <Link to='/login'><button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button></Link>
+                    </div>
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
