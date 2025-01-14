6e407b35c44bb99a86c8d5f935127a34d6d05239
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-01-14 05:35:43 +0530

    Home Page development completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import BuyCredits from './pages/Buycredits'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/result' element={<Result/>}/>
       <Route path='/buy' element={<BuyCredits/>}/>
     </Routes>
+    <Footer/>
     </div>
   )
 }
