1d932907f9a52708edd4f5cb6532de77b71d8e4b
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-01-29 13:44:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
+
 import BuyCredits from './pages/Buycredits'
 import Footer from './components/Footer'
 import Login from './components/Login'
