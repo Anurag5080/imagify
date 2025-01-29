3f78f7959b70b07a80b7ce2d423e3d8b5d81223f
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-01-29 13:44:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
-
 import { Routes, Route } from 'react-router-dom'
 
 import BuyCredits from './pages/Buycredits'
