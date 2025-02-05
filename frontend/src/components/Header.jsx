bfee252225c6915490d4cf8deed0bb0854eda44f
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-02-05 13:37:38 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 
+
 const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center text-center my-20'>
