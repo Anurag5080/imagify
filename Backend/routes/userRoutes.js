dcb0382285ce4fdd91615a7cd514523f26e47b50
Anurag Singh <141731191+Anurag5080@users.noreply.github.com>
2025-02-13 04:05:19 +0530

    Creating Routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {registerUser, loginUser, userCredits} from '../controllers/userController.js'
-import userAuth from '../middleware/auth.js'
+import userAuth from '../middleware/auth.js';
 
 const userRouter = express.Router()
 
