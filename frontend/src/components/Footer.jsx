ad97dcff6a83912ce459cc51f5eca089c5213bbb
Anurag Singh <anurag26official@gmail.com>
2025-04-20 23:38:32 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
+    
     <div className='flex items-center justify-between gap-4 py-3 mt-20 mb-4'>
         <img className='cursor-pointer' src={assets.logo} alt='' width={150}/>
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Anurag Singh | All right reserved.</p>
