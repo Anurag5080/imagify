535b128d6afbad52b4b12d0ca7a7e6456ed620be
Anurag Singh <anurag26official@gmail.com>
2025-05-28 23:22:49 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
+  
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20 mb-4'>
         <img className='cursor-pointer' src={assets.logo} alt='' width={150}/>
