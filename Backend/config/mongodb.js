0f6e044ba3cd6ccf5152d296819afcb7416301c9
Anurag Singh <anurag26official@gmail.com>
2025-08-06 11:41:14 +0530

    Update mongodb.js

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -8,22 +8,5 @@ const connectDB = async ()=>{
 
     await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
 }
-
-
-// import { Client } from "pg";
-
-// const pgClient = new Client("postgresql://neondb_owner:npg_Q2Nb0vUBpxnR@ep-tiny-meadow-a5wi50z4-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
-
-// const pgClient = new Client({
-    //     user: "neondb_owner",
-    //     password: "npg_Q2Nb0vUBpxnR",
-    //     port: "5432",
-    //     host: "ep-tiny-meadow-a5wi50z4-pooler.us-east-2.aws.neon.tech",
-    //     database: "neondb"
-    // })
-    
-    // const connectDB = async()=> {
-        //     await pgClient.connect();
-        // }
         
 export default connectDB;
